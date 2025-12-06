1b8108cb37cc29f30c26259aff39594e85f3026c
Stymphalian <saturnslight@gmail.com>
2025-12-05 22:17:28 -0800

    day06

---
+++ AOC25/Day06.cs
@@ -0,0 +1,109 @@
+// 2025/12/04 9:00
+// 2025/12/04 10:22
+// https://adventofcode.com/2025/day/5
+
+using System.Net.Sockets;
+
+public class Day06 {
+    public const string ADD = "+";
+    public const string MULT = "*";
+    public const char SPACE = ' ';
+
+    Dictionary<int, List<int>> Problems = new();
+    List<string> ProblemLines = new();
+    List<string> Operations = new();
+
+    private void _ReadInput() {
+        string[] lines = File.ReadAllLines("data/input1.txt");
+
+        foreach (string line in lines) {
+            string[] parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            if (parts[0] == ADD || parts[0] == MULT) {
+                Operations = parts.ToList();
+            } else {
+                ProblemLines.Add(line);
+                for (int index = 0; index < parts.Length; index++) {
+                    if (!Problems.ContainsKey(index)) {
+                        Problems[index] = new();
+                    }
+                    Problems[index].Add(int.Parse(parts[index]));
+                }
+            }
+        }
+    }
+
+    public void Solve1() {
+        long answer = 0;
+        foreach (var entry in Problems) {
+            string operation = Operations[entry.Key];
+            long problemResult = operation == ADD ? 0 : 1;
+            foreach (int num in entry.Value) {
+                if (operation == ADD) {
+                    problemResult += num;
+                } else {
+                    problemResult *= num;
+                }
+            }
+
+            Console.WriteLine($"Index = {entry.Key}, Numbers = {entry.Value[0]}, Got = {problemResult}");
+
+            answer += problemResult;
+        }
+
+        Console.WriteLine(answer);
+    }
+
+    public void Solve2() {
+        long ApplyOp(List<int> problemNumbers, string operation) {
+            // Process the collected numbers
+            long problemResult = operation == ADD ? 0 : 1;
+            foreach (int num in problemNumbers) {
+                if (operation == ADD) {
+                    problemResult += num;
+                } else {
+                    problemResult *= num;
+                }
+            }
+            return problemResult;
+        }
+
+        long finalAnswer = 0;
+        List<int> problemNumbers = [];
+        int opIndex = Operations.Count - 1;
+        int currentCol = ProblemLines[0].Length - 1;
+        int numRows = ProblemLines.Count;
+        while (currentCol >= 0) {
+            // Read off the num from the column and add to problem numbers
+            List<char> digits = new();
+            bool allSpaces = true;
+            for (int row = 0; row < numRows; row++) {
+                if (ProblemLines[row][currentCol] == SPACE) { continue; }
+                allSpaces = false;
+                digits.Add(ProblemLines[row][currentCol]);
+            }
+
+            if (allSpaces) {
+                // We reached the space boundary for a problem
+                // Process the last group of numbers we have collected and 
+                // reset the state.
+                finalAnswer += ApplyOp(problemNumbers, Operations[opIndex]);
+                problemNumbers.Clear();
+                opIndex -= 1;
+            } else {
+                int columnNumber = int.Parse(string.Join("", digits));
+                problemNumbers.Add(columnNumber);
+            }
+
+            currentCol -= 1;
+        }
+
+        finalAnswer += ApplyOp(problemNumbers, Operations[opIndex]);
+
+        Console.WriteLine(finalAnswer);
+    }
+
+    public void Run() {
+        _ReadInput();
+        Solve2();
+    }
+}
--- AOC25/Program.cs
+++ AOC25/Program.cs
@@ -2,7 +2,7 @@
 public static class Program {
     public static void Main(string[] args) {
         Console.WriteLine("Advent of Code 2025 - Day 25");
-        Day05 day = new();
+        Day06 day = new();
         day.Run();
     }
 }
